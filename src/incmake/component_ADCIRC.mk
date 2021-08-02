c3af55552ea342f42fc2d2f59b6586dd17056981
pvelissariou1 <panagiotis.velissariou@noaa.gov>
2021-08-02 08:59:13 -0500

    cleanup of ADCIRC/PAHM components

--- src/incmake/component_ADCIRC.mk
+++ src/incmake/component_ADCIRC.mk
@@ -7,6 +7,7 @@ all_component_mk_files+=$(adcirc_mk)
 # Location of source code and installation
 ADCIRC_SRCDIR?=$(ROOTDIR)/ADCIRC
 ADCIRC_BINDIR?=$(ROOTDIR)/ADCIRC_INSTALL
+NUOPC_SRCDIR?=$(ROOTDIR)/ADCIRC/thirdparty/nuopc
 
 # Make sure the expected directories exist and are non-empty:
 $(call require_dir,$(ADCIRC_SRCDIR),ADCIRC source directory)
@@ -28,8 +29,8 @@ build_ADCIRC: $(adcirc_mk)
 
 
 $(adcirc_mk): configure $(CONFDIR)/configure.nems   
-	+$(MODULE_LOGIC) ; cd $(ADCIRC_SRCDIR)/thirdparty/nuopc; exec ./make_nuopc.sh $(comp_option)
-	+$(MODULE_LOGIC) ; cd $(ADCIRC_SRCDIR)/thirdparty/nuopc ; exec $(MAKE) $(ADCIRC_ALL_OPTS) -f makefile.adc_cap.nuopc nuopcinstall \
+	+$(MODULE_LOGIC) ; cd $(NUOPC_SRCDIR); exec ./make_nuopc.sh $(comp_option)
+	+$(MODULE_LOGIC) ; cd $(NUOPC_SRCDIR) ; exec $(MAKE) $(ADCIRC_ALL_OPTS) -f makefile.adc_cap.nuopc nuopcinstall \
           DESTDIR=/ "INSTDIR=$(ADCIRC_BINDIR)"
 	@echo ""
 	test -d "$(ADCIRC_BINDIR)"
@@ -42,11 +43,11 @@ $(adcirc_mk): configure $(CONFDIR)/configure.nems
 # Rule for cleaning the SRCDIR and BINDIR:
 
 clean_ADCIRC_NUOPC:
-	+cd $(ADCIRC_SRCDIR)/thirdparty/nuopc; exec rm -f *.o *.mod
+	+cd $(NUOPC_SRCDIR); exec rm -f *.o *.mod
 	@echo ""
 
 distclean_ADCIRC_NUOPC: clean_ADCIRC_NUOPC
-	+cd $(ADCIRC_SRCDIR)/thirdparty/nuopc ; exec rm -f libadc_cap.a adcirc.mk
+	+cd $(NUOPC_SRCDIR) ; exec rm -f libadc_cap.a adcirc.mk
 	@echo ""
 
 clean_ADCIRC: clean_ADCIRC_NUOPC
